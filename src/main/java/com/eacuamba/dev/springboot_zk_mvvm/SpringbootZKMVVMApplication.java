6260571ba46f80aaa867ae8fe5fb3f4571a67093
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-07-31 01:23:45 +0200

    #initial: Adicionando os ficheiros no repositório.

---
+++ src/main/java/com/eacuamba/dev/springboot_zk_mvvm/SpringbootZKMVVMApplication.java
@@ -0,0 +1,13 @@
+package com.eacuamba.dev.springboot_zk_mvvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootZKMVVMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootZKMVVMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eacuamba/dev/springboot_zk_mvvm/SpringbootZKMVVMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eacuamba.dev.springboot_zk_mvvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootZKMVVMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
